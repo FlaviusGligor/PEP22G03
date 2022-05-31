599bbef2181334b0e58973611d26a449ae8bdcaa
Flavius <flavius.gligor@yahoo.com>
2022-05-31 21:25:22 +0300

    Primu Proiect

---
+++ modul1/part1.py
@@ -0,0 +1,27 @@
+# print('Hello world\n', 'My Object', end='\n', sep='-abcd-')
+#
+# print('print')
+#
+# print('######')
+# print('#    #'),
+# print('######')
+#
+# print(type('Hello world)'))
+#
+# print(type(3))
+
+# print(3+3)
+# print('3' + '3')
+# print(3*3)
+# print('3'*5)
+#
+# print('#'*30)
+# print('#',' '*28,'#',sep='')
+# print('#'*30)
+
+my_value = 'My Object'
+my_value = 15
+my_value2 = 10
+# print(my_value)
+
+print(my_value * my_value2)
