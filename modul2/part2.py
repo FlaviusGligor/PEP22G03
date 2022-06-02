a53e148374c65e2d04c9bc6b52476244d3562e57
FlaviusGligor <flavius.gligor@yahoo.com>
2022-06-02 22:37:33 +0300

    Modul2

---
+++ modul2/part2.py
@@ -0,0 +1,67 @@
+# # INT
+# number = 3
+# print(type(number))
+#
+# number = int('3')
+# print('Nr as text', type(number))
+#
+# # float
+# number = 3.1
+# print(type(number))
+#
+# number = float('3')
+# print('Nr as text', type(number))
+#
+# type = False
+# print('Adress for id')
+
+
+
+#parola = 7710
+#parola1 = input("Introduceti parola ")
+#print(parola==int(parola1))
+
+#prob 2
+# numar1 = int(input('Primul numar:'))
+# numar2 = int(input('Al doilea numar:'))
+# print('Media numerelor este:',(numar1+numar2)/2)
+# print('Impartirea numerelor:',int(numar1/numar2))
+# print('ridicarea la putere:',numar1**numar2)
+# print('Media numerelor este:',(numar1+numar2)/2,'Impartirea numerelor:',int(numar1/numar2),'ridicarea la putere:',numar1**numar2)
+#
+# print('prob.4')
+# venit = int(input('Venit:'))
+# print('50% = ',(50/100)*venit)
+# print('30% = ',(30/100)*venit)
+# print('20% = ',(20/100)*venit)
+#
+# my_number = 0 + 4j
+# print(type(my_number))
+# print(my_number)
+#
+# print(2/3) # 0.(6)
+#
+# #radical
+# my_number = (-1)**(1/2)
+# print(my_number)
+
+# a=3
+# b=4
+# c=5
+# print('rezultatul este:', (-b+((b**2-4*a*c)**(1/2)))/(2*a))
+#
+
+# print('True and False is: ' , True and False)
+# print('True and False is: ' , True or False)
+# print('True and False is: ' , 1 or 0)
+# print('True and False is: ' , 1 and 0)
+
+# print('True xor 1 is: ', True.__xor__(1))
+# print('True xor 1 is: ', True.__xor__(True))
+# print(not (True and True))
+# print('a'.__add__('b'))
+
+# print(not True and True)
+#
+# print (not True and False)
+# print (not(True and False))
